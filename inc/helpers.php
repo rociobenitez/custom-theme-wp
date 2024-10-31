5f9cafc810f2032a18b51a62e62e3c838979d941
Rocio Benitez <rociobenitez2403@gmail.com>
2024-10-31 19:17:08 +0100

    feat: añadir función de paginación personalizada y organizar helpers en archivo helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -45,3 +45,40 @@ function tagTitleFaq($h, $titulo, $class, $id = '', $class2 = '', $content = '')
         $content ? ' ' . esc_html($content) : ''
     );
 }
+
+// Función personalizada de paginación
+function custom_pagination($total_pages = null, $current_page = null) {
+   // Si no se define el total de páginas, intenta obtenerlo de la consulta global
+   if (!$total_pages) {
+      global $wp_query;
+      $total_pages = $wp_query->max_num_pages;
+   }
+
+   // Si no hay múltiples páginas, no se muestra la paginación
+   if ($total_pages <= 1) {
+      return;
+   }
+
+   // Determina la página actual si no se pasa como argumento
+   if (!$current_page) {
+      $current_page = max(1, get_query_var('paged'));
+   }
+
+   // Genera la URL base de la primera página y elimina posibles argumentos adicionales
+   $base = strtok(get_pagenum_link(1), '?');
+
+   // Argumentos para la función de paginación
+   $args = array(
+      'base'      => $base . '%_%',
+      'format'    => 'page/%#%/', 
+      'current'   => $current_page,
+      'total'     => $total_pages,
+      'prev_text' => __('« Anterior'),
+      'next_text' => __('Siguiente »'),
+      'end_size'  => 1,
+      'mid_size'  => 2,
+   );
+
+   // Mostrar la paginación
+   echo paginate_links($args);
+}
