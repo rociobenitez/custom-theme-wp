da5c1f482c83fe617eb712a32917091931d7080f
Rocio Benitez <rociobenitez2403@gmail.com>
2024-12-27 09:26:54 +0100

    update: ajustado el ancho del contenido en dispositivos móviles

--- template-parts/blocks/simple_text_content.php
+++ template-parts/blocks/simple_text_content.php
@@ -3,44 +3,37 @@
  * Sección: Texto Centrado sin Imagen
  */
 
-$tagline        = $block['tagline'] ?? '';
-$htag_tagline   = $block['htag_tagline'] ?? 3;
-$title          = $block['title'] ?? '';
-$htag_title     = $block['htag_title'] ?? 2;
-$description    = $block['description'] ?? '';
-$bg_color       = !empty($block['background_color']) ? $block['background_color'] : '#FFFFFF';
-$cta_button     = $block['button'] ?? null;
-$col_main_text  = !empty($block['width_content']) ? $block['width_content'] : 'col-8';
-$text_align     = !empty($block['text_align']) ? $block['text_align'] : 'text-center';
-$block_align    = $block['text_align'] == 'text-center'
-    ? 'justify-content-center align-items-center'
-    : ($block['text_align'] == 'text-left'
-        ? 'justify-content-start align-items-start'
-        : ($block['text_align'] == 'text-right'
-            ? 'justify-content-end align-items-end'
-            : ''));
+$tagline       = $bloque['tagline'] ?? '';
+$htag_tagline  = $bloque['htag_tagline'] ?? 3;
+$title         = $bloque['title'] ?? '';
+$htag_title    = $bloque['htag_title'] ?? 2;
+$text          = $bloque['text'] ?? '';
+$bg_color      = !empty($bloque['background_color']) ? $bloque['background_color'] : '#FFFFFF';
+$cta_button    = $bloque['button'] ?? null;
+$col_main_text = !empty($bloque['width_content']) ? $bloque['width_content'] : 'col-lg-8';
+$text_align    = !empty($bloque['text_align']) ? $bloque['text_align'] : 'text-center';
+$block_align   = $bloque['text_align'] == 'text-center'
+   ? 'justify-content-center align-items-center'
+   : ($bloque['text_align'] == 'text-left'
+      ? 'justify-content-start align-items-start'
+      : ($bloque['text_align'] == 'text-right'
+         ? 'justify-content-end align-items-end'
+         : ''));
 ?>
-<section class="alternating-block py-5 my-5 bg-custom" style="background-color: <?php echo esc_attr($bg_color); ?>">
+<section class="simple-text-block py-5 my-5 bg-custom" style="background-color: <?php echo esc_attr($bg_color); ?>">
    <div class="container">
-      <div class="row <?php echo $block_align; ?> gap-5 gap-lg-0">
-           <!-- Texto -->
-           <div class="<?php echo $col_main_text; ?> <?php echo esc_attr($text_align); ?>">
-            <?php if (!empty($tagline)):
-               echo tagTitle($htag_tagline, $tagline, 'tagline', '');
-            endif; 
-            if (!empty($title)):
-               echo tagTitle($htag_title, $title, 'heading-2', '');
-            endif; ?>
-            <div class="block-description"><?php echo $description; ?></div>
-
-            <?php if ($cta_button): ?>
-               <a href="<?php echo esc_url($cta_button['url']); ?>" 
-                  class="btn btn-lg btn-primary mt-3" 
-                  target="<?php echo esc_attr($cta_button['target']); ?>">
-                  <?php echo esc_html($cta_button['title']); ?>
-               </a>
-            <?php endif; ?>
-         </div>
-      </div>
+      <?php
+      get_component('template-parts/components/section-heading', [
+         'show_heading'     => true,
+         'tagline'          => $tagline,
+         'htag_tagline'     => $htag_tagline,
+         'title'            => $title,
+         'htag_title'       => $htag_title,
+         'text'             => $text,
+         'block_align'      => $block_align . 'gap-5 gap-lg-0',
+         'container_class'  => 'col-11' . $col_main_text,
+         'text_align_class' => $text_align
+      ]);
+      ?> 
    </div>
 </section>
