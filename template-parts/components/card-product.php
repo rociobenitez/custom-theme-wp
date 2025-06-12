1e475c6044e62975874e32fba24c8b31cdc2571d
Rocío <rociobenitez2403@gmail.com>
2025-06-12 13:29:11 +0200

    feat: optimizada la card de producto

--- template-parts/components/card-product.php
+++ template-parts/components/card-product.php
@@ -1,44 +1,33 @@
 <?php
 /**
- * Componente: Product Card
- * 
+ * Card Product Component
+ *
  * Variables esperadas:
- * - $product_obj: Objeto del producto (`WC_Product`).
- * - $product_id: ID del producto.
- * - $htag_products: Nivel de etiqueta HTML para el título del producto.
- * - $default_product_img: Imagen predeterminada si no hay miniatura.
- * - $show_price: Booleano para mostrar el precio.
+ *  - product (WC_Product)
+ *  - show_price (bool)
  */
+if ( ! defined('ABSPATH') ) exit;
 
-// Validar que se pase el objeto del producto
-if (!isset($product_obj) || !isset($product_id)) {
-    return; // Salir si faltan datos necesarios
-}
+use Custom_Theme\Helpers\Template_Helpers;
+
+$product    = $args['product'];
+$show_price = ! empty( $args['show_price'] );
 ?>
-<div class="product-item card border-0">
-   <a href="<?php echo esc_url(get_permalink($product_id)); ?>" class="product-link">
-      <!-- Imagen del producto -->
-      <div class="product-image">
-         <?php if (has_post_thumbnail($product_id)) : ?>
-            <?php echo get_the_post_thumbnail($product_id, 'medium', ['class' => 'card-img-top rounded']); ?>
-         <?php elseif ($default_product_img) : ?>
-            <img src="<?php echo esc_url($default_product_img); ?>" class="card-img-top rounded"
-               alt="<?php echo esc_attr(get_the_title($product_id)); ?>" >
-         <?php else : ?>
-            <img src="<?php echo esc_url(wc_placeholder_img_src()); ?>" class="card-img-top rounded"
-               alt="<?php echo esc_attr(get_the_title($product_id)); ?>" >
-         <?php endif; ?>
-      </div>
 
-      <!-- Detalles del producto -->
-      <div class="card-body px-0">
-         <?php echo tagTitle($htag_products, get_the_title($product_id), 'heading-6 product-title', ''); ?>
-
-         <?php if ($show_price && $product_obj->get_price_html()) : ?>
-            <p class="product-price text-muted mb-0">
-               <?php echo $product_obj->get_price_html(); ?>
-            </p>
-         <?php endif; ?>
-      </div>
-   </a>
+<div class="col-6 col-md-4 col-lg-3">
+  <div class="card h-100 border-0">
+    <a href="<?php echo esc_url( $product->get_permalink() ); ?>">
+      <?php echo $product->get_image( 'medium', ['class'=>'card-img-top rounded-0'] ); ?>
+    </a>
+    <div class="card-body text-center">
+      <a href="<?php echo esc_url( $product->get_permalink() ); ?>" class="text-decoration-none">
+         <?php echo Template_Helpers::tag_title( $args['htag'], esc_html( $product->get_name() ), 'card-title fs-6' ); ?>
+      </a>
+      <?php if ( $show_price ) : ?>
+        <div class="card-price">
+          <?php echo wp_kses_post( $product->get_price_html() ); ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
 </div>
