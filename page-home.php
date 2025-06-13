6c42a66e1f1b18094290df7a77f6a2e43b6ce9a3
Rocio Benitez <rociobenitez2403@gmail.com>
2025-06-13 12:20:09 +0200

    WIP: guardar cambios locales

--- page-home.php
+++ page-home.php
@@ -15,14 +15,12 @@ $fields = function_exists('get_fields') ? get_fields() : [];
 // Hero (si existe fondo imagen o video)
 if ( ! empty( $fields['hero_background_image'] ) 
   || ! empty( $fields['hero_background_video'] ) ) {
-
   get_template_part( 'template-parts/blocks/hero', null, $fields );
 }
 
 // Slider
 if ( ! empty( $fields['slides'] ) 
   && is_array( $fields['slides'] ) ) {
-
   get_template_part( 'template-parts/blocks/slider', null, [ 'slides' => $fields['slides'] ] );
 }
 
